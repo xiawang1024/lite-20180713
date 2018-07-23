dec6190cd26faeff73f9d5c005815dadb1a776b8
DESKTOP-BE6T737\xiawa <xiawang1024@icloud.com>
2018-07-23 17:52:56 +0800

    feat:节目单互动小程序

---
+++ pages/comment/comment.js
@@ -0,0 +1,163 @@
+// pages/comment/comment.js
+const innerAudioContext = wx.createInnerAudioContext()
+innerAudioContext.autoplay = true
+
+innerAudioContext.onPlay(() => {
+  console.log('开始播放')
+})
+innerAudioContext.onError((res) => {
+  console.log(res.errMsg)
+  console.log(res.errCode)
+})
+const recorderManager = wx.getRecorderManager()
+const options = {
+  duration: 10000,
+  sampleRate: 44100,
+  numberOfChannels: 1,
+  encodeBitRate: 192000,
+  format: 'aac',
+  frameSize: 50
+}
+const util = require('../../utils/util.js')
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    anchorIsFixed:false,
+    isShowProgram:true,
+    percent:0,
+    currentTime:'00:00',
+    duration:'00:00',
+    isPlay:true
+  },
+  onPageScroll(options) {
+    let {scrollTop} = options
+    if(scrollTop >= 100) {
+      this.setData({
+        anchorIsFixed:true
+      })
+    }else {
+      this.setData({
+        anchorIsFixed: false
+      })
+    }
+  },
+  openProgram() {
+    this.setData({
+      isShowProgram:false
+    })
+  },
+  closeProgram() {
+    this.setData({
+      isShowProgram: true
+    })
+  },
+  chooseImage() {
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths
+      }
+    })
+  },
+  seekAudio(event) {    
+    let { value } = event.detail
+    let seekTime = innerAudioContext.duration * value / 100;    
+    innerAudioContext.seek(seekTime)
+    innerAudioContext.play()
+    this.setData({
+      isPlay: true
+    })
+  },
+  switchPlayStatus() {
+    if(innerAudioContext.paused) {
+      innerAudioContext.play()
+      this.setData({
+        isPlay:true
+      })
+    }else {
+      innerAudioContext.pause()
+      this.setData({
+        isPlay: false
+      })
+    }
+  },
+  audioPause() {
+    innerAudioContext.pause()
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    innerAudioContext.src = 'http://stream.hndt.com/vod/_definst_/mp4:radio/20180723/xinwen_20180723_0600_0630.m4a/playlist.m3u8';
+    innerAudioContext.play()
+    innerAudioContext.onTimeUpdate(() => {
+      let { duration, currentTime } = innerAudioContext
+      let percent = (currentTime / duration * 100).toFixed(2);      
+      console.log(percent)
+      duration = util.formatPlayTime(duration)
+      currentTime = util.formatPlayTime(currentTime)      
+      this.setData({
+        currentTime,
+        duration,
+        percent
+      })
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
---
+++ utils/util.js
@@ -0,0 +1,27 @@
+const formatTime = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+const formatPlayTime = interval => {
+  interval = interval | 0
+  const minute = formatNumber(interval / 60 | 0)
+  const second = formatNumber(interval % 60)
+  return `${minute}:${second}`
+}	
+
+module.exports = {
+  formatTime: formatTime,
+  formatPlayTime
+}
